357dc1c4794aca755b04fc96ca841bd81c4262a7
abdus-sabur <absabur929@gmail.com>
2023-10-04 12:33:26 +0600

    sdfksjflkjsfk

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ const categoryRouter = require("./routes/categoryRoute");
 // app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload())
 app.use(cookieParser())
-app.use(express.json({ limit: "50mb" })); 
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json()); 
+app.use(express.urlencoded({extended: true }));
 
 app.use("/api/product",productRouter)
 app.use("/api/user",userRouter)

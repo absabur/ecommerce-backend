e71ed13ad2b2addff400507fa21581cd2b8abe16
abdus-sabur <absabur929@gmail.com>
2023-10-05 13:42:44 +0600

    addnig cors

--- app.js
+++ app.js
@@ -9,9 +9,11 @@ const cookieParser = require("cookie-parser")
 const bodyParser = require('body-parser')
 const fileUpload = require('express-fileupload');
 const categoryRouter = require("./routes/categoryRoute");
+const cors = require('cors')
 
 // app.use(express.json());
 // app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors())
 app.use(fileUpload())
 app.use(cookieParser())
 app.use(express.json()); 

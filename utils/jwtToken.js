d6d03d8bb05b1e76a55b9f392450e27e6c1c6226
abdus-sabur <absabur929@gmail.com>
2023-10-05 15:36:12 +0600

    cookie secure true

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -2,7 +2,7 @@ exports.jwtToken = (token,res) => {
     return res.cookie("access_token", token, {
         maxAge: Number(process.env.COOKIE_EXPIRE)*24*60*60*1000,
         httpOnly: true,
-        secure: false,
+        secure: true,
         sameSite: 'none',
     })
 }
